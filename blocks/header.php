1de4e0b72efa2e11850f88116dc0c5f943d69235
ArtjomsTatagaskins <Nova-III@yandex.ru>
2024-11-21 16:19:26 +0200

    Reg/auth connected to db

--- blocks/header.php
+++ blocks/header.php
@@ -1,15 +1,16 @@
 <header>
     <div class="web-name"><a href="/mainpage.php"><h3>Interaktīvā karte</h3></a></div>
-    <div class="dropdown-list">
+    <div class="link-list">
 
         <?php
-        if(isset($_COOKIE['login'])) {
-            echo '<a href="/user.php">Personīgais konts</a>';
-            if (basename($_SERVER['PHP_SELF']) == 'user.php') {
+        if (isset($_COOKIE['login'])) {
+            if (basename($_SERVER['PHP_SELF']) != 'user.php') {
+                echo '<a href="/user.php">Personīgais konts</a>';
+            } else {
                 echo '
-                    <form action="/lib/logout.php" method="post">
-                        <button type="submit" class="logout-button">Iziet no sistēmas</button>
-                    </form>';
+            <form action="/lib/logout.php" method="post">
+                <button type="submit" class="logout-button">Iziet no sistēmas</button>
+            </form>';
             }
         } else {
             echo '<a href="/authorization.php">Reģistrācija/Ieiešana</a>';
